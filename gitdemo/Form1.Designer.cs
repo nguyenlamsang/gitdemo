0a6aac78c8637b5cda3da2052465e87ebfce75cb
SangNguyen <sangnguyen12cb7@gmail.com>
2021-03-24 14:02:18 +0700

    Nhom b : changed lbusername

--- gitdemo/Form1.Designer.cs
+++ gitdemo/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbusername = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtuser = new System.Windows.Forms.TextBox();
             this.txtpass = new System.Windows.Forms.TextBox();
@@ -46,15 +46,15 @@
             this.label1.Text = "ĐĂNG NHẬP";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // lbusername
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(219, 190);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "TÀI KHOẢN";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lbusername.AutoSize = true;
+            this.lbusername.Location = new System.Drawing.Point(219, 190);
+            this.lbusername.Name = "lbusername";
+            this.lbusername.Size = new System.Drawing.Size(82, 17);
+            this.lbusername.TabIndex = 1;
+            this.lbusername.Text = "TÀI KHOẢN";
+            this.lbusername.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -98,7 +98,7 @@
             this.Controls.Add(this.txtpass);
             this.Controls.Add(this.txtuser);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbusername);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -110,7 +110,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbusername;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtuser;
         private System.Windows.Forms.TextBox txtpass;
